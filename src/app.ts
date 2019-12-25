b9532f682bb2903c32c8a4a79a03af35f7f6da1b
Pedro Prieto <pedro.prieto003@gmail.com>
2019-12-24 20:09:26 -0500

    More initial configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, json } from "express";
+import cors from "cors";
 
 const app: Application = express();
+
+// GLOBAL MIDDLEWARE
+app.use(cors())
+app.use(json());
+
 
 app.get("/", (req: Request, res: Response) : void => {
   res.send("Hello World");
