d4f0bb88472364fccd7a8ff2c8bfacb80f7d34f0
Pedro Prieto <pedro.prieto003@gmail.com>
2020-01-06 22:31:36 -0500

    Added extra error handling

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 
-
 const userRoute = require("./routes/userRoutes");
-
-
-
-
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 app.get("/", (req, res) => res.send("Hello World"));
 app.use("/api/v1/user", userRoute);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({ error: `Can't find ${req.originalUrl}` });
+});
 
 module.exports = app;
