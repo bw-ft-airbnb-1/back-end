fe08580eb8d0d3a8bb6513a20956f000c91eac6d
Pedro Prieto <pedro.prieto003@gmail.com>
2020-01-07 13:43:29 -0500

    Did get by user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,8 @@ const {
   checkEditBody,
   updateUser,
   deleteUser,
-  getAllUsers
+  getAllUsers,
+  getOneUser
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -19,15 +20,12 @@ router.post("/register", checkBodyAndHashPass, addUser);
 router.post("/signin", checkSignInBody, checkIfUserExistsByEmail, signIn);
 router
   .route("/user")
-  .put(
-    getToken,
-    checkEditBody,
-    updateUser
-  )
+  .get(getToken, getOneUser)
+  .put(getToken, checkEditBody, updateUser)
   .delete(getToken, deleteUser);
 
 //// ONLY FOR DEV
-if(process.env.NODE_ENV === "development"){
+if (process.env.NODE_ENV === "development") {
   router.route("/users").get(getAllUsers);
 }
 
