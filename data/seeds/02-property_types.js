8d65d3c4c99da6e11254c45fc424cb0bccfd6724
Pedro Prieto <pedro.prieto003@gmail.com>
2020-01-09 16:29:01 -0500

    Added all property types

--- data/seeds/02-property_types.js
+++ data/seeds/02-property_types.js
@@ -5,16 +5,25 @@ class PropertyType {
 }
 exports.seed = function(knex) {
   return knex("property_types").insert([
-    new PropertyType("apartment"),
-    new PropertyType("loft"),
     new PropertyType("house"),
-    new PropertyType("hostel"),
     new PropertyType("townhouse"),
-    new PropertyType("guest suite"),
+    new PropertyType("apartment"),
+    new PropertyType("condominium"),
+    new PropertyType("loft"),
+    new PropertyType("serviced apartment"),
+    new PropertyType("hostel"),
+    new PropertyType("bed and breakfast"),
     new PropertyType("guest house"),
     new PropertyType("hotel"),
+    new PropertyType("boutique hotel"),
+    new PropertyType("bungalow"),
     new PropertyType("boat"),
+    new PropertyType("tiny house"),
+    new PropertyType("houseboat"),
+    new PropertyType("campter/rv"),
     new PropertyType("villa"),
+    new PropertyType("aparthotel"),
+    new PropertyType("guest suite"),
     new PropertyType("other")
   ]);
 };
