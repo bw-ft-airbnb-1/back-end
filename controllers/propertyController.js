a96ab6583bf1096d03796fc9e12f641ea2b41e58
Pedro Prieto <pedro.prieto003@gmail.com>
2020-01-09 14:47:46 -0500

    New amenities and create a property

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,6 +1,8 @@
 const { catchAsync } = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const Property = require("../models/propertyModel");
+const { check, validationResult } = require("express-validator");
+const faker = require("faker");
 
 /// GET PROPERTY BY PROPERTY ID - RETURNS PROPERTY WITH ALL OPTIONS
 exports.getPropertyById = catchAsync(async (req, res) => {
@@ -10,7 +12,81 @@ exports.getPropertyById = catchAsync(async (req, res) => {
   res.status(200).json(property);
 });
 
-exports.newProperty = catchAsync(async (req, res) => {});
+exports.newProperty = catchAsync(async (req, res) => {
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return next(new AppError(err.errors, 401));
+  }
+  if (!req.body.image) {
+    req.body.image = faker.image.image();
+  }
+  const {
+    minimum_nights,
+    bedrooms,
+    bathrooms,
+    security_deposit,
+    zip_code,
+    accommodates,
+    image,
+    price
+  } = req.body;
+
+  const property = {
+    ...req.types,
+    minimum_nights,
+    bedrooms,
+    bathrooms,
+    security_deposit,
+    zip_code,
+    accommodates,
+    image,
+    price,
+    user_id: req.userID
+  };
+
+  const amenities = req.amenities;
+  const newProperty = await Property.createProperty(property, amenities);
+  return res.status(200).json(newProperty);
+});
+
+exports.editProperty = catchAsync(async (req, res) => {
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return next(new AppError(err.errors, 401));
+  }
+  if (!req.body.image) {
+    req.body.image = faker.image.image();
+  }
+  const {
+    id,
+    minimum_nights,
+    bedrooms,
+    bathrooms,
+    security_deposit,
+    zip_code,
+    accommodates,
+    image,
+    price
+  } = req.body;
+
+  const property = {
+    id,
+    ...req.types,
+    minimum_nights,
+    bedrooms,
+    bathrooms,
+    security_deposit,
+    zip_code,
+    accommodates,
+    image,
+    price,
+    user_id: req.userID
+  };
+
+  const amenities = req.amenities;
+  const newProperty = await Property.createProperty(property, amenities);
+  return res.status(200).json(newProperty);
+});
 
 /// DELETES PROPERTY BY PROPERTY ID
 exports.deleteProperty = catchAsync(async (req, res) => {
@@ -43,9 +119,50 @@ exports.validatePropertyID = catchAsync(async (req, res, next) => {
   next();
 });
 
-exports.validatePropertyRights = (req, res, next) => {
-  if (req.property.id !== req.userID) {
-    throw new AppError("Not Authorized", 403);
+exports.getForeignKeys = catchAsync(async (req, res, next) => {
+  const { bed_type, room_type, property_type, amenities } = req.body;
+  if (typeof amenities !== "object") {
+    return next(new AppError("Please provide amenities"));
   }
+  if (!bed_type || !room_type || !property_type) {
+    return next(
+      new AppError("Please provide bed_types, room_types, and property_types")
+    );
+  }
+  if (Object.keys(amenities).length != 0) {
+    const amenitiesRes = await Property.checkAmenities(amenities);
+    req.amenities = amenitiesRes;
+  }
+
+  const typesObj = await Property.checkTypes({
+    bed_type,
+    room_type,
+    property_type
+  });
+  req.types = typesObj;
   next();
-};
+});
+
+exports.propertyValidator = [
+  check("minimum_nights")
+    .isInt([{ gt: 0, lt: 31 }])
+    .withMessage("Enter minimum nights 0 - 31"),
+  check("bedrooms")
+    .isInt([{ gt: 1, lt: 12 }])
+    .withMessage("Enter number of bedrooms 1 - 12"),
+  check("bathrooms")
+    .isInt([{ gt: 1, lt: 6 }])
+    .withMessage("Enter number of bedrooms 1 - 6"),
+  check("security_deposit")
+    .isInt([{ gt: 1, lt: 1500 }])
+    .withMessage("Enter security deposit 1 - 1500"),
+  check("zip_code")
+    .isInt([{ gt: 10000, lt: 99999 }])
+    .withMessage("Enter a valid zip code"),
+  check("accommodates")
+    .isInt([{ gt: 1, lt: 13 }])
+    .withMessage("Enter accommodates 1 - 13"),
+  check("price")
+    .isInt()
+    .withMessage("Please provide the price provided by ds")
+];
--- data/seeds/06-amenities.js
+++ data/seeds/06-amenities.js
@@ -4,16 +4,60 @@ class Amenities {
   }
 }
 
+const amenities = [
+  "wifi",
+  "heating",
+  "kitchen",
+  "essentials",
+  "washer",
+  "hair dryer",
+  "laptop friendly workspace",
+  "hangers",
+  "iron",
+  "hot water",
+  "shampoo",
+  "tv",
+  "family",
+  "kid friendly",
+  "internet",
+  "host greets you",
+  "smoke detector",
+  "buzzer",
+  "wireless intercom",
+  "free street parking",
+  "refrigerator",
+  "dishes and silverware",
+  "bed linens",
+  "cooking basics",
+  "stove",
+  "lock on bedroom door",
+  "oven",
+  "elevator",
+  "coffee maker",
+  "smoking allowed",
+  "first aid kit",
+  "cable tv",
+  "dishwasher",
+  "long term stays allowed",
+  "luggage dropoff allowed",
+  "dryer",
+  "fire extinguisher",
+  "pets allowed",
+  "extra pillows and blankets",
+  "patio or balcony",
+  "microwave",
+  "private entrance",
+  "paid parking off premises",
+  "safety card",
+  "free parking on premises",
+  "private living room",
+  "bathtub"
+];
+const amenitiesArr = [];
+amenities.map(amenity => {
+  amenitiesArr.push(new Amenities(amenity));
+});
+
 exports.seed = function(knex) {
-  return knex("amenities").insert([
-    new Amenities("laundry"),
-    new Amenities("spa"),
-    new Amenities("wifi"),
-    new Amenities("cleaning"),
-    new Amenities("breakfast"),
-    new Amenities("security"),
-    new Amenities("pool"),
-    new Amenities("patio"),
-    new Amenities("pets allowed")
-  ]);
+  return knex("amenities").insert(amenitiesArr);
 };
